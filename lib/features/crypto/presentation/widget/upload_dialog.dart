ce23afe7335317519320b5d076ca588248082750
Dsoji <dami.osoj@gmail.com>
2025-04-27 10:12:36 +0100

    feat: updated dialog

--- lib/features/crypto/presentation/widget/upload_dialog.dart
+++ lib/features/crypto/presentation/widget/upload_dialog.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:mdiho/common/widgets/custom_buttons.dart';
@@ -46,24 +47,74 @@ void showCryptoDialog({
 
           return Dialog(
             backgroundColor: theme.brightness == Brightness.dark
-                ? AppColors.secondaryColor.shade500
+                ? AppColors.darkBorder
                 : AppColors.whiteColor.shade100,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
             child: Padding(
-              padding: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(4),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  const Text("Step 2",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
-                  const SizedBox(height: 8),
-                  const Text(
-                    "Once you’ve sent the BTC, upload your proof of payment below:",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 14),
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: theme.brightness == Brightness.dark
+                          ? AppColors.secondaryColor.shade700
+                          : AppColors.whiteColor
+                              .shade100, // or adjust for dark mode if needed
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            const Expanded(
+                              child: Divider(
+                                color: Colors.grey,
+                                thickness: 1,
+                                endIndent: 8,
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 12, vertical: 4),
+                              decoration: BoxDecoration(
+                                color: theme.brightness == Brightness.dark
+                                    ? AppColors.darkBorder
+                                    : AppColors.whiteColor.shade100,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: const Text(
+                                "Step 2",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ),
+                            const Expanded(
+                              child: Divider(
+                                color: Colors.grey,
+                                thickness: 1,
+                                indent: 8,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Gap(8),
+                        const Text(
+                          "Once you’ve sent the BTC, upload your proof of payment below:",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 12,
+                          ),
+                        ),
+                        const SizedBox(height: 12),
+                      ],
+                    ),
                   ),
                   const SizedBox(height: 12),
                   InfoWidget(
@@ -84,7 +135,7 @@ void showCryptoDialog({
                         ),
                         borderRadius: BorderRadius.circular(10),
                         color: theme.brightness == Brightness.dark
-                            ? Colors.transparent
+                            ? AppColors.darkBorder
                             : AppColors.greyColor.shade50,
                       ),
                       padding: const EdgeInsets.all(12),
@@ -191,10 +242,11 @@ void showCryptoDialog({
                     text: 'Submit Proof',
                     width: double.infinity,
                     height: 48,
-                    onPressed: () => showTransactionDialog(
-                      context,
-                      onSecondaryAction,
-                    ),
+                    onPressed: () {},
+                    //  => showTransactionDialog(
+                    //   context,
+                    //   onSecondaryAction,
+                    // ),
                     textColor: Colors.white,
                     color: AppColors.primaryColor.shade500,
                   ),
